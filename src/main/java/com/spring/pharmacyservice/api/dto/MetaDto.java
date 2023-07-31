67cba4cefb8e01e465757da2038f87e59bed2783
JadeKim042386 <jk042386@gmail.com>
2023-07-31 14:43:23 +0900

    feat: Implementation kakao retrival address api - Add Dtos - Add RestTemplate Bean - Add Service for URI building and address searching feature  #3

---
+++ src/main/java/com/spring/pharmacyservice/api/dto/MetaDto.java
@@ -0,0 +1,15 @@
+package com.spring.pharmacyservice.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaDto {
+    @JsonProperty("total_count")
+    private Integer totalCount;
+}
